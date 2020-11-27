ad5b6c57c6d43cf75dbbb2f9c1fe6631b513a378
x.zhang <griffcho16@gmail.com>
2020-11-27 22:25:34 +0900

    added static folder, temnplates, and also user authentication methods

--- smallBusiness/settings.py
+++ smallBusiness/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'userRegistration',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
--- userRegistration/views.py
+++ userRegistration/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import RegisterForm
+
 
 # Create your views here.
-def signUpPage(reqeust):
-    return HttpResponse('this is login page')
+def signUpPage(response):
+    if response.method == "POST":
+        form = RegisterForm(response.POST)
+        if form.is_valid():
+            form.save()
+
+        return redirect("www.google.com")
+
+    else:
+	    form = RegisterForm()
+
+    return render(response, "userRegistration/signup.html", {"form":form})
