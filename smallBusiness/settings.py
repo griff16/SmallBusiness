e0d35c79d1d382bfb13d6defc0bcce54ab0c8095
x.zhang <griffcho16@gmail.com>
2020-11-27 20:16:00 +0900

    update

--- smallBusiness/settings.py
+++ smallBusiness/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'userRegistration',
 ]
 
@@ -50,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.contrib.sites',
 ]
 
 ROOT_URLCONF = 'smallBusiness.urls'
