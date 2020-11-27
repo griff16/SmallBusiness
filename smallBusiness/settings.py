cc06973b79576204753154c61e590cef2f2dc9b0
x.zhang <griffcho16@gmail.com>
2020-11-27 20:14:46 +0900

    update

--- smallBusiness/settings.py
+++ smallBusiness/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.sites',
 ]
 
 ROOT_URLCONF = 'smallBusiness.urls'
