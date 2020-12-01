ac2e47d8f305f3672a3b00377635cc2f342c6c9d
x.zhang <griffcho16@gmail.com>
2020-12-01 21:24:15 +0900

    added db.json

--- smallBusiness/settings.py
+++ smallBusiness/settings.py
@@ -8,6 +8,9 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
+
+this is the url for how to convert sqlite to postgresql
+https://www.shubhamdipt.com/blog/django-transfer-data-from-sqlite-to-another-database/
 """
 
 import os
